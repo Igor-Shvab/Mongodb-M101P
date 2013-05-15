44e9150db78a6cb60ad6a7704c4a60dc579e10ab
Aleksander Balakirev <balakirevs@i.ua>
2013-05-15 10:13:51 +0200

    Added final exam

---
+++ Final/final-6.js
@@ -0,0 +1,19 @@
+{
+	"_id" : ObjectId("50c598f582094fb5f92efb96"),
+	"first_name" : "John",
+	"last_name" : "Doe",
+	"date_of_admission" : ISODate("2010-02-21T05:00:00Z"),
+	"residence_hall" : "Fairweather",
+	"has_car" : true,
+	"student_id" : "2348023902",
+	"current_classes" : [
+		"His343",
+		"Math234",
+		"Phy123",
+		"Art232"
+	]
+}
+
+answers: Set w=0, j=0 on writes
+
+         Remove all indexes from the collection
---
+++ Week_6/Hw6-1/hw6-1.js
@@ -0,0 +1,3 @@
+Which of the following statements are true about MongoDB replication. 
+- The minimum sensible number of voting nodes to a replica set is three.
+- The oplog utilizes a capped collection.
